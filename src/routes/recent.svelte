bdfa0804e17826618525a95af9093d2f31bcd078
shajidhasan <shajid6063@gmail.com>
2021-08-21 01:55:32 +0600

    first commit

---
+++ src/routes/recent.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Recent</title>
+</svelte:head>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<h2 class="text-3xl uppercase text-white font-light flex justify-center w-full px-4 h-92">
+	Nothing Found...
+</h2>
+<br />
+<br />
+<Footer />
---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+/** @type {import('@sveltejs/kit').Config} */
+// import vercel from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter()
+	}
+};
+
+export default config;
